8c2ef013e0b9d58b18cc1bc64eb0b94794a5560b
gagannaidu <gagan.globin76@gmail.com>
2017-04-24 18:39:13 +0530

    Form in a View

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -10,8 +10,8 @@ class SignUpForm(forms.ModelForm):
         email = self.cleaned_data.get('email')
         email_base, provider = email.split("@")
         domain, extension = provider.split('.')
-        if not domain =='USC':
-            raise forms.ValidationError("Please make sure you use your USC email")
+        # if not domain =='USC':
+        #     raise forms.ValidationError("Please make sure you use your USC email")
         if not extension == 'edu':
             raise forms.ValidationError("Please use a valid .edu email")
         return email
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     title = "my title"
+    form = SignUpForm(request.POST or None)
     context = {
         "title":title,
+        "form":form,
     }
+    # if request.method== "POST":
+    #     print request.POST
+    
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        #print request.POST   #not recomended
+        # print instance.email
+        # print instance.timestamp
+        # print instance.full_name
+        context = {
+            "title": "Thank you"
+        }
+    
     return render(request, "home.html",context)

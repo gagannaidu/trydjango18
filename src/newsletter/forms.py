b5121be7704a6ef2bd2681c620c1fb4ecd6dd2bc
gagannaidu <gagan.globin76@gmail.com>
2017-04-24 15:26:51 +0530

    Form validation

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -5,5 +5,15 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['email','full_name']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split('.')
+        if not domain =='USC':
+            raise forms.ValidationError("Please make sure you use your USC email")
+        if not extension == 'edu':
+            raise forms.ValidationError("Please use a valid .edu email")
+        return email
         
         
